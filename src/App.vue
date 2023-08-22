481b7c85375649add4f514aee7bb1d5259d45ddf
marcusfu <marcussfu@gmail.com>
2023-08-23 01:03:51 +0800

    add quasar and tailwindcss and quasar card system

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
+let roomArray = reactive([]);
+
+let arrayFromApi = [
+  {
+    id: 1,
+    name: "AA",
+    dealerImageSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 2,
+    name: "BB",
+    dealerImageSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 3,
+    name: "CC",
+    dealerImageSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 4,
+    name: "DD",
+    dealerImageSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 5,
+    name: "EE",
+    dealerImageSrc: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+];
+
+roomArray.push(...arrayFromApi);
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,10 +43,36 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card v-for="room in roomArray" :key="room.id" class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ room.name }}
+        </q-card-section>
+
+        <q-img class="col-5" :src="room.dealerImageSrc" />
+      </q-card-section>
+    </q-card>
+    <q-card v-ripple class="my-box cursor-pointer q-hoverable">
+     <!-- <span class="q-focus-helper"></span> -->
+     <q-card-section>
+        ...
+     </q-card-section>
+</q-card>
+  </div>
 </template>
 
 <style scoped>
+.my-card {
+  width: 100%;
+  max-width: 400px;
+}
+
+.my-card:hover {
+  max-width: 450px;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
